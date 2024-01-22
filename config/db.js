569439168a72826310167c6f6ea89767c156bc2b
Alejandro <alejobetancur2@gmail.com>
2024-01-22 11:08:23 -0500

    pruebas

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         timestamps: true // Crea dos columnas más en la tabla: createdAt y updatedAt.
     },
     pool: { // Configuración para la conexión a la base de datos.
-        max: 5, // Máximo de conexiones simultáneas.
+        max: 20, // Máximo de conexiones simultáneas.
         min: 0, // Mínimo de conexiones simultáneas.
         acquire: 30000, // Tiempo máximo, en milisegundos, que se intentará establecer una conexión antes de arrojar un error.
         idle: 10000 // Tiempo máximo, en milisegundos, que una conexión puede estar inactiva antes de ser liberada.
