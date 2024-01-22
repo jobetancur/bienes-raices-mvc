36c7ded64ce5ff94de75ebc8c749805586426eb0
Alejandro <alejobetancur2@gmail.com>
2024-01-22 15:53:18 -0500

    nueva prueba

--- models/RealEstate.js
+++ models/RealEstate.js
@@ -49,10 +49,6 @@ const RealEstate = db.define('realestates', {
         allowNull: false,
         defaultValue: 0
     },
-    userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-    },
     createdAt: Sequelize.DATE,
     updatedAt: Sequelize.DATE,
 });
